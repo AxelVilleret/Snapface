18f4306f7c3b9c49bc6cd01911124e6c559e63ed
Axel Villeret <91218852+AxelVilleret@users.noreply.github.com>
2022-11-01 12:13:49 +0100

    first commit

---
+++ frontend/src/app/core/services/face-snaps.service.ts
@@ -0,0 +1,50 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
+import { FaceSnap } from '../models/face-snap.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FaceSnapsService {
+
+
+  constructor(private http: HttpClient) { }
+
+  getAllFaceSnaps(): Observable<FaceSnap[]> {
+    return this.http.get<FaceSnap[]>('http://localhost:3000/facesnaps');
+  }
+
+  getFaceSnapById(faceSnapId: number): Observable<FaceSnap> {
+    return this.http.get<FaceSnap>(`http://localhost:3000/facesnaps/${faceSnapId}`);
+  }
+
+  snapFaceSnapById(faceSnapId: number, snapType: 'snap' | 'unsnap'): Observable<FaceSnap> {
+    return this.getFaceSnapById(faceSnapId).pipe(
+      map(FaceSnap => ({
+        ...FaceSnap,
+        snaps: snapType === 'snap' ? FaceSnap.snaps + 1 : FaceSnap.snaps - 1
+      })),
+      switchMap(updatedFaceSnap => this.http.put<FaceSnap>(`http://localhost:3000/facesnaps/${faceSnapId}`, updatedFaceSnap))
+    );
+  }
+
+  addFaceSnap(formValue: { title: string, description: string, imageUrl: string, location?: string }): Observable<FaceSnap> {
+    return this.getAllFaceSnaps().pipe(
+      map(facesnaps => [...facesnaps].sort((a, b) => a.id - b.id)),
+      map(sortedFacesnaps => sortedFacesnaps[sortedFacesnaps.length - 1]),
+      map(previousFacesnap => ({
+        ...formValue,
+        snaps: 0,
+        createdDate: new Date(),
+        id: previousFacesnap.id + 1
+      })),
+      switchMap(newFacesnap => this.http.post<FaceSnap>(
+        'http://localhost:3000/facesnaps',
+        newFacesnap)
+      )
+    );
+  }
+
+}
---
+++ frontend/src/app/face-snaps/components/face-snap-list/face-snap-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { interval, Observable, Subject } from 'rxjs';
+import { concatMap, mergeMap, delay, exhaustMap, map, switchMap, take, tap, takeUntil } from 'rxjs/operators';
+import { FaceSnap } from '../../../core/models/face-snap.model';
+import { FaceSnapsService } from '../../../core/services/face-snaps.service';
+
+@Component({
+  selector: 'app-face-snap-list',
+  templateUrl: './face-snap-list.component.html',
+  styleUrls: ['./face-snap-list.component.scss']
+})
+export class FaceSnapListComponent implements OnInit {
+
+  faceSnaps$!: Observable<FaceSnap[]>;
+
+  constructor(private faceSnapsService: FaceSnapsService) { }
+
+  ngOnInit(): void {
+    this.faceSnaps$ = this.faceSnapsService.getAllFaceSnaps();
+  }
+
+}
